b00e6f5346887c3f4c61de8aa1386f848b44e472
Dima Moskalenko <dmtrmsklnk@gmail.com>
2022-08-05 22:05:24 +0300

    new kata

--- CodeWars.js
+++ CodeWars.js
@@ -749,3 +749,19 @@ function addBinary(a,b) {
   let num = a + b;
   return Number(num).toString(2);
   }
+
+/* 34  If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers. */
+
+/* my solution */
+let countSheep = function (num){
+  let str = ''
+  for (let i = 1; i <= num; i++) {
+    if(num > 0){
+     str += `${i} sheep...`
+    }
+  }
+  return str;
+}
