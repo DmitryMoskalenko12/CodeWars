797eb2ef93672a7a0d6394c218299bd37d9cabf1
Dima Moskalenko <dmtrmsklnk@gmail.com>
2022-11-09 22:11:39 +0200

    add task delete copy

--- CodeWars.js
+++ CodeWars.js
@@ -812,3 +812,29 @@ const func = (str) => {
   }
 }
 console.log(func('aabba')) 
+
+/* Написать функцию которая возвращает массив
+ только с уникальными значениями
+ [1,1,2,2,3,4,5,5] => [3,4] */
+
+function withoutRepeat(arr) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+   let count = 0;
+
+   for (let j = 0; j < arr.length; j++){ 
+        /* 0  */   /* 01234 */ 
+     if (arr[i] === arr[j]) {
+      count++
+     } 
+   }
+
+    if (count === 1) {
+      result.push(arr[i])
+    }
+  }
+  return result
+}
+console.log(withoutRepeat([1,1,2,2,3,4,5,5]))
+
+
