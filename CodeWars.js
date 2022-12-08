e30fb0cfaee0ea238d2d48277458adfbf0dec6de
Dima Moskalenko <dmtrmsklnk@gmail.com>
2022-12-08 23:20:35 +0200

    new task

--- CodeWars.js
+++ CodeWars.js
@@ -837,4 +837,14 @@ function withoutRepeat(arr) {
 }
 console.log(withoutRepeat([1,1,2,2,3,4,5,5]))
 
+/* As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
 
+The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+For example:
+
+gimme([2, 3, 1]) => 0 */
+function gimme (triplet) {
+  const arr = [...triplet].sort((a, b) => a - b)
+  return triplet.indexOf(arr[1])
+  }
