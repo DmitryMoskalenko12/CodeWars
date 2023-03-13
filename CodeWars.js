1bbce96f06729bccdb74e634c683bee5319dba72
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-03-14 00:09:05 +0200

    add new task

--- CodeWars.js
+++ CodeWars.js
@@ -2623,3 +2623,71 @@ console.log(text.truncate({  length: 20, omission: ' more...'}))
       return this.history;
     }
   }
+
+
+ /*  Щороку Mate Robot Factory проводить бої роботів на арені. Різні модифікації роботів, змагаються за першість, а чемпіони попадають на дошку слави. Тут і військові роботи, і роботи-прибиральники, і вантажні роботи, як бачиш, яких роботів тут тільки немає.
+
+  Давай, щоб усе було чесно, створимо дошку слави для цих змагань (клас BoardOfFame).
+  
+  За замовчуванням, місць на дошці слави буде всього 3. Але в випадку міжнародних турнірів, де чемпіонських місць знадобиться більше, конструктор класу BoardOfFame зможе прийняти будь-яке інше число місць.
+  
+  Створи метод addRecord для усіх екземплярів класу BoardOfFame, який прийматиме об'єкт robot, та додаватиме новий запис на дошку слави. Якщо новий робот матиме більше балів, ніж інші роботи, він займе своє місце в топі, посунувши інших роботів до низу.
+  
+  Також створи getter list для екземплярів, який буде виводити список чемпіонів у форматі з прикладу. Список має бути рядком. Getter повинен повертати ... для не існуючих елементів (якщо потрібно повернути перші 5, але в масиві всього 3 робота).
+  
+  За правилами турнірів усі роботи мають мати унікальні імена. Якщо новий робот має таке ж ім'я, як один з роботів в списку - просто онови у існуючого робота значення score.
+  
+  Приклад:
+  
+  const board = new BoardOfFame();
+  board.list // `1. ... | 2. ... | 3. ...`
+  board.addRecord({name: 'Cleaner-900', score: 6})
+  board.addRecord({name: 'Cleaner-775', score: 16})
+  board.list // `1. Cleaner-775: 16 | 2. Cleaner-900: 6 | 3. ...`
+  board.addRecord({name: 'MX-56', score: 23})
+  board.addRecord({name: 'Cleaner-775', score: 30})
+  board.list // `1. Cleaner-775: 30 | 2. MX-56: 23 | 3. Cleaner-900: 6`
+  
+  const internationalBoard = new BoardOfFame(5);
+  internationalBoard.list = `1. ... | 2. ... | 3. ... | 4. ... | 5. .. */
+
+  class BoardOfFame {
+    // Write your code here
+    constructor(size = 3) {
+      this.size = size;
+      this.records = [];
+    }
+  
+    addRecord(robot) {
+      const { name, score } = robot;
+      const existingRecord = this.records.find((record) => record.name === name);
+  
+      if (existingRecord) {
+        existingRecord.score = score;
+      } else {
+        this.records.push(robot);
+      }
+  
+      this.records.sort((a, b) => b.score - a.score);
+  
+      if (this.records.length > this.size) {
+        this.records = this.records.slice(0, this.size);
+      }
+    }
+  
+    get list() {
+      let listStr = '';
+  
+      for (let i = 0; i < this.size; i++) {
+        if (i >= this.records.length) {
+          listStr += `${i + 1}. ... | `;
+        } else {
+          const { name, score } = this.records[i];
+  
+          listStr += `${i + 1}. ${name}: ${score} | `;
+        }
+      }
+  
+      return listStr.slice(0, -3);
+    }
+  }
