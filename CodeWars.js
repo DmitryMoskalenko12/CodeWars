90612baab40bef7d6aa9d23900a004c8e59825f8
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-06-20 11:40:46 +0300

    add new task

--- CodeWars.js
+++ CodeWars.js
@@ -2708,3 +2708,20 @@ function descendingOrder(n){
   })
   return Number(arrNumber.join('')); 
 }
+
+/* Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+Examples
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+don't worry about uppercase vowels
+y is not considered a vowel for this kata */
+
+function shortcut (string) {
+  const arr = ['a', 'e', 'i', 'o', 'u'];
+  const res = string.split('').filter((item, i) => !arr.find(letter => letter === item))
+  return res.join('');
+}
+console.log(shortcut('never'))
